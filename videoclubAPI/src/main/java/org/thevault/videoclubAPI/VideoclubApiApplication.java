d7e01b0702f4ffff5a92f52c7ccc02cff38a9c83
lizbello <blizethguadalupe@gmail.com>
2025-07-14 12:13:18 -0600

    The API project has been created

---
+++ videoclubAPI/src/main/java/org/thevault/videoclubAPI/VideoclubApiApplication.java
@@ -0,0 +1,13 @@
+package org.thevault.videoclubAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoclubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoclubApiApplication.class, args);
+	}
+
+}
---
+++ videoclubAPI/src/test/java/org/thevault/videoclubAPI/VideoclubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thevault.videoclubAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoclubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
