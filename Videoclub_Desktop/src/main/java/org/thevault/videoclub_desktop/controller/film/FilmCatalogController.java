d2a747396e564036a7d3cdb810ee5e67bd81d714
lizbello <blizethguadalupe@gmail.com>
2025-07-29 15:33:22 -0600

    Frontend for film catalog done

---
+++ Videoclub_Desktop/src/main/java/org/thevault/videoclub_desktop/controller/film/FilmCatalogController.java
@@ -0,0 +1,80 @@
+package org.thevault.videoclub_desktop.controller.film;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
+
+
+public class FilmCatalogController {
+    @FXML
+    private TilePane catalogContainer;
+
+    @FXML
+    private ImageView ivProfilePicture;
+
+    @FXML
+    void onAdventureClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onComedyClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDramaClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onFamilyClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onForeignClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onHistoricalClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onHorrorClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMisteryClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMusicalClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onReturnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRomanceClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onScifiClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSuperheroClicked(ActionEvent event) {
+
+    }
+}
